76ccb8f1e18314021268f3c560fd7bb2ff928651
Mongalmoy Karmakar <mongalmoykarmakar.bwn@gmail.com>
2022-01-02 19:26:50 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+
+const app = express();
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get('/register', function(req, res){
+    res.render('register');
+});
+
+app.get('/login', function(req, res){
+    res.render('login');
+});
+
+
+
+app.listen(3000, function(){
+    console.log("Server started on port 3000");
+});
